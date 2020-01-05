0820dac2ccc18ff53f1579ba15f15585d50bfd68
Srinath N.S <srinath1@users.noreply.github.com>
2020-01-05 20:25:14 +0100

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,21 +5,32 @@ import { Context as LocationContext} from '../context/LocationContext'
 import MapView ,{Polyline}from 'react-native-maps'
 
 const Map=()=>{
-    const { currentLocation}=useContext(LocationContext)
+    const { state:{currentLocation}}=useContext(LocationContext)
     
-    console.log(currentLocation)
+    
     
     
     if(!currentLocation){
         return <ActivityIndicator  size="large"  style={{marginTop:200}} />
     }  
     
-    return <MapView 
-    style={styles.map}
-    
+    return <MapView style={styles.map}
+    initialRegion={{
 
+        ...currentLocation.coords,
+        
+        latitudeDelta:0.01,
+        longitudeDelta:0.01
+    }}
+    region={{
+        ...currentLocation.coords,
+        
+        latitudeDelta:0.01,
+        longitudeDelta:0.01
+    }}
     
-     />
+    >
+        </MapView>
         
 
          
